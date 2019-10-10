fb85c3b4dc86de04e9d3257a600ffa860dbeee14
Morteza <Morteza@DESKTOP-GTKGRP7>
2019-10-10 14:39:09 +0330

    Initialize project.

---
+++ Issue1034/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Issue1034
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
